b1bf2c7badc4367460b7c4778446967b6d0a1f5b
ahuo <ahuo@umich.edu>
2020-12-22 18:36:17 -0500

    another

--- script.js
+++ script.js
@@ -8,31 +8,52 @@
 //     console.log('success');
 // }
 
-var input = document.getElementById("playcode");
-function getLength() {
-    return input.value.length;
-};  
-function keyPress() {
-    if (getLength() > 0 && event.which === 13) {
-        window.alert("yes");
-      saveToFirebase(input.value);
-    }
-};
+// var input = document.getElementById("playcode");
+// function getLength() {
+//     return input.value.length;
+// };  
+// function keyPress() {
+//     if (getLength() > 0 && event.which === 13) {
+//         window.alert("yes");
+//       saveToFirebase(input.value);
+//     }
+// };
   
+var config = {
+    apiKey: "AIzaSyBWQZYESrRt2aryonDptE9voG6an0f3sAs",
+    authDomain: "trial-b8d07.firebaseapp.com",
+    projectId: "trial-b8d07",
+    storageBucket: "trial-b8d07.appspot.com",
+    messagingSenderId: "464877621417",
+    appId: "1:464877621417:web:78c8d9d5a29afd719bd9b8",
+    measurementId: "G-X58DME4P64"
+};
+  firebase.initializeApp(config);
+  firebase.database().goOnline();
 
-function saveToFirebase(email) {
-    var emailObject = {
-        email: email
-    };
+// Get a reference to the database service
+var database = firebase.database();
 
-    firebase.database().ref('subscription-entries').push().set(emailObject)
-        // .then(function(snapshot) {
-        //     success(); // some success method
-        // }, function(error) {
-        //     // console.log('error' + error);
-        //     error(); // some error method
-        // });
+function pressButton() {
+    window.alert("Wenaaaa sotooo")
+    firebase.database().ref().child("Value").set("Some value")
 }
 
-input.addEventListener("keypress", keyPress);
+// var database = firebase.database();
+
+// function saveToFirebase(email) {
+//     var emailObject = {
+//         email: email
+//     };
+
+//     firebase.database().ref('subscription-entries').push().set(emailObject)
+//         // .then(function(snapshot) {
+//         //     success(); // some success method
+//         // }, function(error) {
+//         //     // console.log('error' + error);
+//         //     error(); // some error method
+//         // });
+// }
+
+// input.addEventListener("keypress", keyPress);
 // saveToFirebase(email);
