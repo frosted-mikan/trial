c99e226524835af85a66dc81b64ac409ad4f4b31
ahuo <ahuo@umich.edu>
2020-12-22 17:37:01 -0500

    add stuff

---
+++ script.js
@@ -0,0 +1,37 @@
+function success() {
+    window.alert("Success!");
+    console.log('error');
+}
+
+function error() {
+    window.alert("Failure!");
+    console.log('success');
+}
+
+var input = document.getElementById("playcode");
+function getLength() {
+    return input.value.length;
+};  
+function keyPress() {
+    if (getLength() > 0 && event.which === 13) {
+      saveToFirebase(input.value);
+    }
+};
+  
+
+function saveToFirebase(email) {
+    var emailObject = {
+        email: email
+    };
+
+    firebase.database().ref('subscription-entries').push().set(emailObject)
+        .then(function(snapshot) {
+            success(); // some success method
+        }, function(error) {
+            // console.log('error' + error);
+            error(); // some error method
+        });
+}
+
+input.addEventListener("keypress", keyPress);
+saveToFirebase(email);
